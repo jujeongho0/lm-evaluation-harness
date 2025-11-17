a7e365f93d5be8dfe233f12f8b11488e3b02b7c2
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-11-17 16:15:35 +0900

    Create run_evaluate_wbl_20b_a2b.sh

---
+++ run_evaluate_wbl_20b_a2b.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+MODEL_PATH="/path/to/WBL-20B-A2B-HF"
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks mmlu \
+    --num_fewshot 5 \
+    --batch_size auto:4 \
+    --trust_remote_code \
+    --output_path results \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks kmmlu \
+    --num_fewshot 5 \
+    --batch_size auto:4 \
+    --trust_remote_code \
+    --output_path results \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks kobest \
+    --num_fewshot 10 \
+    --batch_size auto:4 \
+    --trust_remote_code \
+    --output_path results \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks gpqa_main_n_shot \
+    --num_fewshot 5 \
+    --batch_size auto:4 \
+    --trust_remote_code \
+    --output_path results \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks mbpp \
+    --num_fewshot 3 \
+    --batch_size auto:4 \
+    --output_path results \
+    --trust_remote_code \
+    --confirm_run_unsafe_code \
