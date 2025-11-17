8f3340222628f8e0bf5c72354cb41262a3bb9c62
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-11-17 16:09:35 +0900

    Create run_evaluate_kimi_linear_48b_a3b.sh

---
+++ run_evaluate_kimi_linear_48b_a3b.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# pip install tiktoken
+# pip install flash-linear-attention
+
+export HF_HUB_CACHE="/path/to/.cache/huggingface/hub"
+
+MODEL_PATH="moonshotai/Kimi-Linear-48B-A3B-Base"
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks mmlu \
+    --num_fewshot 5 \
+    --batch_size auto:4 \
+    --output_path results \
+    --trust_remote_code \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks kmmlu \
+    --num_fewshot 5 \
+    --batch_size auto:4 \
+    --output_path results \
+    --trust_remote_code \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks kobest \
+    --num_fewshot 10 \
+    --batch_size auto:4 \
+    --output_path results \
+    --trust_remote_code \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks gpqa_main_n_shot \
+    --num_fewshot 5 \
+    --batch_size auto:4 \
+    --output_path results \
+    --trust_remote_code \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks mbpp \
+    --num_fewshot 3 \
+    --batch_size auto:4 \
+    --output_path results \
+    --confirm_run_unsafe_code \
+    --trust_remote_code \
