423e301404a8694c9f860e0bd1602b5eae73cbaa
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-12-01 14:58:13 +0900

    Update run_evaluate_wbl_100b_a10b.sh

--- run_evaluate_wbl_100b_a10b.sh
+++ run_evaluate_wbl_100b_a10b.sh
@@ -6,8 +6,22 @@ MODEL_PATH="/path/to/WBL-100B-A10B-HF"
 
 lm_eval --model hf \
     --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
-    --tasks leaderboard_mmlu_pro,leaderboard_bbh,leaderboard_gpqa,leaderboard_math_hard,leaderboard_musr \
-    --batch_size auto:4 \
+    --tasks leaderboard_mmlu_pro \
+    --batch_size auto \
+    --trust_remote_code \
+    --output_path results \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks leaderboard_bbh \
+    --batch_size auto \
+    --trust_remote_code \
+    --output_path results \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks leaderboard_gpqa \
+    --batch_size auto \
     --trust_remote_code \
     --output_path results \
 
@@ -15,7 +29,7 @@ lm_eval --model hf \
     --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
     --tasks kmmlu_redux \
     --num_fewshot 5 \
-    --batch_size auto:4 \
+    --batch_size auto \
     --trust_remote_code \
     --output_path results \
 
@@ -23,7 +37,14 @@ lm_eval --model hf \
     --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
     --tasks kobest \
     --num_fewshot 10 \
-    --batch_size auto:4 \
+    --batch_size auto \
+    --trust_remote_code \
+    --output_path results \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks leaderboard_math_hard \
+    --batch_size auto \
     --trust_remote_code \
     --output_path results \
 
@@ -31,7 +52,14 @@ lm_eval --model hf \
     --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
     --tasks mbpp \
     --num_fewshot 3 \
-    --batch_size auto:4 \
+    --batch_size auto \
     --output_path results \
     --trust_remote_code \
     --confirm_run_unsafe_code \
+
+lm_eval --model hf \
+    --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
+    --tasks leaderboard_musr \
+    --batch_size auto \
+    --trust_remote_code \
+    --output_path results \
