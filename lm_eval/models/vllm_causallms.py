4d1e3cc65520bc45f5e7dd685d4258a6d911176a
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-12-17 16:24:54 +0900

    Update vllm_causallms.py

--- lm_eval/models/vllm_causallms.py
+++ lm_eval/models/vllm_causallms.py
@@ -320,7 +320,7 @@ class VLLM(TemplateLM):
             )
 
             # FIXME
-            thinking = "OK."
+            thinking = ""
             chat_templated = chat_templated + f"{thinking}\n</think>\n\n"
             
         except jinja2.exceptions.TemplateError:
