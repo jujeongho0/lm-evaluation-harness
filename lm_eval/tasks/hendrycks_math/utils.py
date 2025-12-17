487a601f576af35ea74785b19208fb32e5cb32ea
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-12-17 22:08:17 +0900

    Update utils.py

--- lm_eval/tasks/hendrycks_math/utils.py
+++ lm_eval/tasks/hendrycks_math/utils.py
@@ -23,7 +23,7 @@ def process_results(doc: dict, results: List[str]) -> Dict[str, int]:
     else:
         answer = results[0][indices[0] + 1 : indices[-1]]
 
-    if is_equiv(answer, remove_boxed(last_boxed_only_string(doc["solution"]))):
+    if is_equiv(remove_boxed(last_boxed_only_string(answer)), remove_boxed(last_boxed_only_string(doc["solution"]))): # FIXME
         retval = 1
 
     results = {
