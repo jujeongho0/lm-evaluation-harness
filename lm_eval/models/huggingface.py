4906ca4ae614f99effdf9f6c0e5047e798167de9
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-12-17 19:17:18 +0900

    Update huggingface.py

--- lm_eval/models/huggingface.py
+++ lm_eval/models/huggingface.py
@@ -1534,8 +1534,7 @@ class HFLM(TemplateLM):
             )
             
             # FIXME
-            thinking = ""
-            chat_templated = chat_templated + f"{thinking}\n</think>\n\n"
+            chat_templated = chat_templated + "\n</think>\n\n"
 
         except jinja2.exceptions.TemplateError:
             eval_logger.warning(
