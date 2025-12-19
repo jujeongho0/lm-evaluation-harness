e862a75245aab9d68b0316679e6c0d865d00dea8
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-12-20 04:29:26 +0900

    Create run_vllm_evaluate_wbl_instruct.sh

---
+++ run_vllm_evaluate_wbl_instruct.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+export VLLM_WORKER_MULTIPROC_METHOD=spawn
+export HF_HUB_CACHE="/path/to/.cache/huggingface/hub"
+
+MODEL_PATH="/path/to/wbl_model"
+
+lm_eval --model vllm \
+    --model_args pretrained=${MODEL_PATH},pipeline_parallel_size=8 \
+    --tasks click \
+    --batch_size auto \
+    --trust_remote_code \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192
+    --output_path results \
+
+lm_eval --model vllm \
+    --model_args pretrained=${MODEL_PATH},pipeline_parallel_size=8 \
+    --tasks hrm8k \
+    --batch_size auto \
+    --trust_remote_code \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192
+    --output_path results \
+
+lm_eval --model vllm \
+    --model_args pretrained=${MODEL_PATH},pipeline_parallel_size=8 \
+    --tasks ifbench \
+    --batch_size auto \
+    --trust_remote_code \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192
+    --output_path results \
+
+lm_eval --model vllm \
+    --model_args pretrained=${MODEL_PATH},pipeline_parallel_size=8 \
+    --tasks ifeval \
+    --batch_size auto \
+    --trust_remote_code \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192
+    --output_path results \
