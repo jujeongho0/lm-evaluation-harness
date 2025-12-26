7b18f9b8efa75055b84506b4b79f4fe1b42782c5
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-12-26 14:37:15 +0900

    Update run_vllm_evaluate_wbl_instruct.sh

--- run_vllm_evaluate_wbl_instruct.sh
+++ run_vllm_evaluate_wbl_instruct.sh
@@ -6,20 +6,18 @@ export HF_HUB_CACHE="/path/to/.cache/huggingface/hub"
 MODEL_PATH="/path/to/wbl_model"
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL_PATH},pipeline_parallel_size=8,thinking_budget=2048,think_end_token="</think>" \
+    --model_args pretrained=${MODEL_PATH},pipeline_parallel_size=8 \
     --tasks click \
     --batch_size auto \
     --trust_remote_code \
-    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192 \
     --apply_chat_template \
     --output_path results \
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL_PATH},pipeline_parallel_size=8,thinking_budget=2048,think_end_token="</think>" \
+    --model_args pretrained=${MODEL_PATH},pipeline_parallel_size=8 \
     --tasks kobalt \
     --batch_size auto \
     --trust_remote_code \
-    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192 \
     --apply_chat_template \
     --output_path results \
     
