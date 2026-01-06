c1a9c38748a2ac249288c0c0f14501a4f70dff17
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-06 23:49:48 +0900

    Update and rename run_hf_evaluate_wbl.sh to run_hf_evaluate_vaetki.sh

--- run_hf_evaluate_vaetki.sh
+++ run_hf_evaluate_vaetki.sh
@@ -2,7 +2,7 @@
 
 export HF_HUB_CACHE="/path/to/.cache/huggingface/hub"
 
-MODEL_PATH="/path/to/wbl_model"
+MODEL_PATH="NC-AI-consortium-VAETKI/VAETKI"
 
 lm_eval --model hf \
     --model_args pretrained=${MODEL_PATH},attn_implementation=flash_attention_2,parallelize=True \
