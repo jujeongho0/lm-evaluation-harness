2b52905db616be33fc365f59a2a2401a0f6d022e
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-06 23:50:59 +0900

    Update run_vllm_evaluate_wbl.sh

--- run_vllm_evaluate_wbl.sh
+++ run_vllm_evaluate_wbl.sh
@@ -3,76 +3,76 @@
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 export HF_HUB_CACHE="/path/to/.cache/huggingface/hub"
 
-MODEL_PATH="/path/to/wbl_model"
+MODEL_PATH="NC-AI-consortium-VAETKI/VAETKI"
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8,enforce_eager=True \
+    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8 \
     --tasks leaderboard_mmlu_pro \
     --batch_size auto \
     --trust_remote_code \
-    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192 \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=32768 \
     --output_path results \
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8,enforce_eager=True \
+    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8 \
     --tasks leaderboard_bbh \
     --batch_size auto \
     --trust_remote_code \
-    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192 \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=32768 \
     --output_path results \
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8,enforce_eager=True \
+    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8 \
     --tasks leaderboard_gpqa \
     --batch_size auto \
     --trust_remote_code \
-    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192 \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=32768 \
     --output_path results \
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8,enforce_eager=True \
+    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8 \
     --tasks kmmlu_redux \
     --batch_size auto \
     --trust_remote_code \
-    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192 \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=32768 \
     --output_path results \
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8,enforce_eager=True \
+    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8 \
     --tasks kobest \
     --num_fewshot 10 \
     --batch_size auto \
     --trust_remote_code \
-    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192 \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=32768 \
     --output_path results \
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8,enforce_eager=True \
+    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8 \
     --tasks leaderboard_math_hard \
     --batch_size auto \
     --trust_remote_code \
-    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192 \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=32768 \
     --output_path results \
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8,enforce_eager=True \
+    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8 \
     --tasks mbpp \
     --batch_size auto \
     --trust_remote_code \
-    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192 \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=32768 \
     --output_path results \
     --confirm_run_unsafe_code \
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8,enforce_eager=True \
+    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8 \
     --tasks leaderboard_musr \
     --batch_size auto \
     --trust_remote_code \
-    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=8192 \
+    --gen_kwargs spaces_between_special_tokens=True,max_gen_toks=32768 \
     --output_path results \
 
 lm_eval --model vllm \
-    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8,enforce_eager=True,max_length=32768 \
+    --model_args pretrained=${MODEL_PATH},tensor_parallel_size=8,max_length=32768 \
     --tasks ruler \
     --batch_size auto \
     --trust_remote_code \
